3ce5f79d6fbac9144d228d3f1a87a4c0ecd516ce
nonti <nonty8@gmail.com>
2025-06-02 14:12:01 +0200

    project start up

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import logo from './assets/logo.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={logo} className="logo" alt="Vite logo" />
+        </a>
+       
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className='bg-blue-500' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
